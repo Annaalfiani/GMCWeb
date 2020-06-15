24e7f7162cd4ee5d41dc3ea26c80e25e53ad295d
annaalfiani <annaalfiani.poltektegal@gmail.com>
2020-06-15 14:21:25 +0700

    fixing in api

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -19,6 +19,23 @@ class OrderController extends Controller
 
     public function order(Request $request)
     {
+        $this->validate($request,[
+           'tanggal' => 'required',
+           'jam' => 'required'
+        ]);
+
+        $checkOrder = Order::where('id_kursi', $request->id_kursi)
+            ->where('id_studio', $request->id_studio)
+            ->where('id_film', $request->id_film)
+            ->where('id_jadwal_tayang', $request->id_jadwal_tayang)
+            ->where('tanggal', $request->tanggal)
+            ->where('jam', $request->jam)
+            ->first();
+
+        if ($checkOrder){
+            return response()->json(['message' => 'kursi pada tanggal dan jam tersebut sudah di pesan orang', 'status' => false]);
+        }
+
         $order = new Order();
         $order->id_customer = Auth::guard('api')->user()->id;
         $order->id_studio = $request->id_studio;
@@ -28,7 +45,6 @@ class OrderController extends Controller
         $order->tanggal = $request->tanggal;
         $order->jam = $request->jam;
         $order->save();
-
 
         return response()->json([
             'message' => 'berhasail order bioskop',
