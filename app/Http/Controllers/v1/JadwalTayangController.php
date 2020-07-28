298484051135293203d906c2ec8f9ac679e62fde
annaalfiani <annaalfiani.poltektegal@gmail.com>
2020-07-28 22:40:16 +0700

    Validasi

--- app/Http/Controllers/v1/JadwalTayangController.php
+++ app/Http/Controllers/v1/JadwalTayangController.php
@@ -26,15 +26,23 @@ class JadwalTayangController extends Controller
         $now = Carbon::now()->format('Y-m-d');
         $jamSekarang = Carbon::now()->format('H:i');
 
-
-
-        $hours = JamTayang::whereHas('date', function ($queryDate)use ($id, $now){
-            $queryDate->whereHas('schedulle', function ($querySchedulle)use ($id){
-                $querySchedulle->whereHas('dataFilm', function ($queryMovie)use ($id){
-                    $queryMovie->where('id', $id);
-                });
-            })->whereDate('tanggal', '>=', $now);
-        })->whereTime('jam', '>', $jamSekarang)->get();
+        if($now){
+            $hours = JamTayang::whereHas('date', function ($queryDate)use ($id, $now){
+                $queryDate->whereHas('schedulle', function ($querySchedulle)use ($id){
+                    $querySchedulle->whereHas('dataFilm', function ($queryMovie)use ($id){
+                        $queryMovie->where('id', $id);
+                    });
+                })->whereDate('tanggal', $now);
+            })->get();
+        }else{
+            $hours = JamTayang::whereHas('date', function ($queryDate)use ($id, $now){
+                $queryDate->whereHas('schedulle', function ($querySchedulle)use ($id){
+                    $querySchedulle->whereHas('dataFilm', function ($queryMovie)use ($id){
+                        $queryMovie->where('id', $id);
+                    });
+                })->whereDate('tanggal','>=', $now);
+            })->whereTime('jam', '>', $jamSekarang)->get();
+        }
 
         return response()->json([
             'mesage' => 'success',
